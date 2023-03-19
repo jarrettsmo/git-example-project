eab57201f05dbd0402a458572808a4ac7a8aa33c
Jarrett Smolarkiewicz <68030206+jarrettsmo@users.noreply.github.com>
2023-03-19 15:49:13 -0500

    Created read_single() method.

--- INF_653/PHP_REST_MYBLOG/index.php
+++ INF_653/PHP_REST_MYBLOG/index.php
@@ -1,5 +1,21 @@
 <?php
 
+/**
+ * Traversy Media PHP OOP REST API Tutorial
+ * #1
+ * PHP REST API From Scratch [1] - Database & Read
+ * https://www.youtube.com/watch?v=OEWXbpUMODk 
+ * 
+ * #2
+ * PHP REST API From Scratch [2] - Single & Create
+ * https://www.youtube.com/watch?v=-nq4UbD0NT8
+ * 
+ * #3
+ * 
+ * 
+ * 
+ */
+
 echo "Testing 123456789";
 
 phpinfo(INFO_MODULES);
