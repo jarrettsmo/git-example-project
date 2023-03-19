ed9f3b01462668d0aa14efbb45963d5496e5cd42
Jarrett Smolarkiewicz <68030206+jarrettsmo@users.noreply.github.com>
2023-03-19 14:47:00 -0500

    Correcting typo in read.php

--- INF_653/PHP_REST_MYBLOG/api/post/read.php
+++ INF_653/PHP_REST_MYBLOG/api/post/read.php
@@ -4,7 +4,7 @@
     header('Content-Type: application/json');
 
     include_once '../../config/Database.php';
-    include_once '../../models/Post.php';
+    include_once '../../models/post.php';
 
     // Instantiate Database and Connect
     $database = new Database();
