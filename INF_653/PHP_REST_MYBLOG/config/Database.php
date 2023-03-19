e27eef3551e6163a692de33493375d04f2b03a32
Jarrett Smolarkiewicz <68030206+jarrettsmo@users.noreply.github.com>
2023-03-19 13:11:36 -0500

    Setup code for database connection and blog posts.

--- INF_653/PHP_REST_MYBLOG/config/Database.php
+++ INF_653/PHP_REST_MYBLOG/config/Database.php
@@ -10,5 +10,15 @@
         // Database Connection
         public function connect() {
             $this->conn = null;
+
+            try {
+                $this->conn = new PDO('msql:host=' . $this->host . ';dbname=' . $this->db_name, 
+                $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch(PDOException $e) {
+                echo 'Connection Error: ' . $e->getMessage();
+            }
+
+            return $this->conn;
         }
     }
