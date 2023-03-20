f695f3dbb070c3c1e79d7ada29cee51da29f582f
Jarrett Smolarkiewicz <68030206+jarrettsmo@users.noreply.github.com>
2023-03-19 19:43:55 -0500

    Finishing create.php.

--- INF_653/PHP_REST_MYBLOG/api/post/create.php
+++ INF_653/PHP_REST_MYBLOG/api/post/create.php
@@ -19,3 +19,22 @@
 
     // Instantiate blog post object
     $post = new Post($db);
+
+    // Get the raw posted data
+    $data = json_decode(file_get_contents("php://input"));
+
+    $post->title = $data->title;
+    $post->body = $data->body;
+    $post->author = $data->author;
+    $post->category_id = $data->category_id;
+
+    // Create post
+    if($post->create()) {
+        echo json_encode(
+            array('message' => 'Post Created')
+        );
+    } else {
+        echo json_encode(
+            array('message' => 'Post Not Created')
+        );
+    }
