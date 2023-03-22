c81fec0127ee3ebbfb0f5ade4a99477623d10c15
Jarrett Smolarkiewicz <68030206+jarrettsmo@users.noreply.github.com>
2023-03-21 23:48:22 -0500

    Corrected code errors.

--- INF_653/PHP_REST_MYBLOG/config/Database.php
+++ INF_653/PHP_REST_MYBLOG/config/Database.php
@@ -1,18 +1,20 @@
 <?php
     class Database {
         // Database Parameters
-        private $host = 'localhost';
-        private $db_name = 'myblog';
-        private $username = 'root';
-        private $password = '';
         private $conn;
 
         // Database Connection
         public function connect() {
             $this->conn = null;
+            // Environment Variables
+            $host = isset($_ENV['TEST_HOST']) ? $_ENV['TEST_HOST'] : '';
+            $db_name = isset($_ENV['TEST_DB']) ? $_ENV['TEST_DB'] : '';
+            $db_port = isset($_ENV['TEST_PORT']) ? $_ENV['TEST_PORT'] : '';
+            $username = isset($_ENV['TEST_USER']) ? $_ENV['TEST_USER'] : '';
+            $password = isset($_ENV['TEST_PW']) ? $_ENV['TEST_PW'] : '';
 
             try {
-                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+                $this->conn = new PDO('mysql:host=' . $host . ';port=' . $db_port . ';dbname=' . $db_name, $username, $password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch(PDOException $e) {
                 echo 'Connection Error: ' . $e->getMessage();
